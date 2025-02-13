81e8b22506430790bd13d4caa64fc8a80c20a463
@amritmohan <amritmohan2284@gmail.com>
2025-02-13 23:52:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function insert_Row() {
 	let cell1 = row.insertCell(0);
 	let cell2 = row.insertCell(1);
 	cell1.innerHTML = "New Cell1";
-	cell1.innerHTML = "New Cell2";
+	cell2.innerHTML = "New Cell2";
 }
